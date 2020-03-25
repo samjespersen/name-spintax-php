594d3ca28f83db26a81416045ac808ec7d3da19f
Sam Jespersen <heyheysamj@gmail.com>
2020-03-24 21:32:44 -0700

    get outta here javascript

--- utils/connect.php
+++ utils/connect.php
@@ -5,15 +5,15 @@ $DBURL = "mysql://qpj6lswa560ip0p7:umffjci4sv5nokll@dno6xji1n8fm828n.cbetxkdyhws
 $connectionString = explode("mysql://", $DBURL);
 $connectionString = explode(":", $connectionString[1]);
 
-$user = $connectionString[0];
-$pass = explode("@", $connectionString[1])[0];
-$db_url = explode("@", $connectionString[1])[1];
-$dbname = explode("/", $connectionString[2])[1];
+// $user = $connectionString[0];
+// $pass = explode("@", $connectionString[1])[0];
+// $db_url = explode("@", $connectionString[1])[1];
+// $dbname = explode("/", $connectionString[2])[1];
 
-// $user = 'root';
-// $pass = '';
-// $db_url = 'localhost';
-// $dbname = 'test';
+$user = 'root';
+$pass = '';
+$db_url = 'localhost';
+$dbname = 'test';
 
 //PDO
 $dsn = 'mysql:host=' . $db_url . ';dbname=' . $dbname;
